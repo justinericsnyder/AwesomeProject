d027473d68a2f1384cafbba55ee34515a0724657
Tyler Matteo <tylermatteo434@gmail.com>
2022-02-01 20:31:28 -0500

    learn the database

--- screens/LeagueChampScrollViewScreen.tsx
+++ screens/LeagueChampScrollViewScreen.tsx
@@ -1,69 +1,66 @@
-import { StyleSheet, SafeAreaView, ScrollView, StatusBar } from 'react-native';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { StyleSheet, SafeAreaView, ScrollView, StatusBar } from "react-native";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
 
-interface Champion
-{
-    key: any,
-    name: string,
-    description: string,
+interface Champion {
+  key: any;
+  name: string;
+  description: string;
 }
-interface Champions extends Array<Champion>{}
+interface Champions extends Array<Champion> {}
 
 const championArray: Champion[] = [
   {
-      key: 0,
-      name: 'Urgot',
-      description: 'he is a crab'
-  }, {
-      key: 1,
-      name: 'Sion',
-      description: 'he is ded man'
-  }, {
-      key: 2,
-      name: 'Kayn',
-      description: 'shitter'
-  }
+    key: 0,
+    name: "Urgot",
+    description: "he is a crab",
+  },
+  {
+    key: 1,
+    name: "Sion",
+    description: "he is ded man",
+  },
+  {
+    key: 2,
+    name: "Kayn",
+    description: "shitter",
+  },
 ];
 
-export default function LeagueChampScrollView({ navigation }: RootTabScreenProps<'Champions'>) {
-    //const champions = [{key: 0, name: 'Urgot', description: 'he is a crab'},{key: 1, name: 'Sion', description: 'he is ded man'},{key: 2, name: 'Kayn', description: 'shitter'}]; 
+export default function LeagueChampScrollView({
+  navigation,
+}: RootTabScreenProps<"Champions">) {
+  //const champions = [{key: 0, name: 'Urgot', description: 'he is a crab'},{key: 1, name: 'Sion', description: 'he is ded man'},{key: 2, name: 'Kayn', description: 'shitter'}];
 
+  function ChampionTitle({ title }: { title: any }) {
+    return <Text style={styles.title}>{title}</Text>;
+  }
 
-    function ChampionTitle(title: any) {
-        return <Text style={styles.title}>{title}</Text>;
-    }
+  function ChampionDescription({ description }: { description: string }) {
+    return <Text style={styles.title}>{description}</Text>;
+  }
 
-    function ChampionDescription(description: any) {
-        return <Text style={styles.title}>{description}</Text>;
-    }
-
-    function ChampionRow(champion: Champion) {
-        return <View style={styles.row}><ChampionTitle title={champion.name}/><ChampionDescription description={champion.description}/></View>;
-    }
-
-    function ChampionList(champions: Champions) {
-        const listItems = champions.map((champion) => {
-            <ChampionRow key={champion.key.toString()} name={champion.name} description={champion.description}/>
-        }
-      );
-      return (
-        <View>
-          {listItems}
-        </View>
-      );
-
-    }
-
-    
-
-    return ( 
-    <ChampionList Champions={championArray} />       
+  function ChampionRow({ champion }: { champion: Champion }) {
+    return (
+      <View style={styles.row}>
+        <ChampionTitle title={champion.name} />
+        <ChampionDescription description={champion.description} />
+      </View>
     );
+  }
+
+  interface ChampionListProps {
+    champions: Champions;
+  }
+
+  function ChampionList({ champions }: ChampionListProps) {
+    const listItems = champions.map((champion) => (
+      <ChampionRow champion={champion} />
+    ));
+    return <View>{listItems}</View>;
+  }
+  return <ChampionList champions={championArray} />;
 }
-
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -72,22 +69,22 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   row: {
     height: 50,
-    width: '100%',
+    width: "100%",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
   scrollView: {
-    backgroundColor: 'pink',
+    backgroundColor: "pink",
     marginHorizontal: 20,
   },
   text: {
     fontSize: 42,
-  }
+  },
 });
