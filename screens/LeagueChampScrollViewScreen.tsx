f0bfdd08f358bc7736c043edd174b4aed8cfde74
justinericsnyder <justinericsnyder@users.noreply.github.com>
2022-02-02 21:28:18 -0500

    Have scrollview working with updated interface for championdata item and next need to work on adding other details on champdata to scrollview.

--- screens/LeagueChampScrollViewScreen.tsx
+++ screens/LeagueChampScrollViewScreen.tsx
@@ -5,17 +5,18 @@ import { RootTabScreenProps } from "../types";
 import React, { useState, useEffect} from 'react'
 import {getAllChampionsNames } from '../services/ddragon/api'
 import _ from "lodash";
+import { ChampionData } from "../services/ddragon/interfaces";
+import { PageContainer } from "./styles";
 
 
 export default function LeagueChampScrollViewScreen({navigation} : RootTabScreenProps < 'Champions' >) {
-const [championData, setChampionData] = useState <Array<string>>([])
+const [championData, setChampionData] = useState <Array<ChampionData>>([])
 
   useEffect(() => {
   const loadChampionData = async () => {
     const champData = await getAllChampionsNames();
-    const headers = _.keys(champData.data);
-    
-    setChampionData(headers);
+    const champArray = _.values(champData.data);
+    setChampionData(champArray);
     }
     loadChampionData()
   }, []) 
@@ -24,7 +25,14 @@ const [championData, setChampionData] = useState <Array<string>>([])
     return(
       <ScrollView contentContainerStyle={styles.container}>
       {championData.length > 0 &&
-         championData.map((key) => <Text key={key} style={styles.title}> {key} </Text>
+         championData.map((champion) => 
+         <View key={champion.key} style={styles.row}>
+          <View style={styles.detailContainer}>
+            <Text style={styles.title}> {champion.id} </Text>
+            <Text style={styles.description}>{champion.title.toString()} </Text>
+           </View>
+           <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"></View>
+         </View>
       )} 
       </ScrollView>
             
@@ -44,12 +52,30 @@ const styles = StyleSheet.create({
     color: 'white'
   },
   separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    marginVertical: 10,
+    height: 5,
+    width: '100%',
+ 
   },
   championImage: {
     height:50,
     width:50,
+  },
+  row: {
+    height: 100,
+    
+  },
+  detailContainer: {
+   paddingLeft:10,
+   paddingTop:10,
+   height:80,
+    
+  },
+  description: {
+    fontSize: 10,
+    fontStyle: 'italic',
+    fontWeight: 'normal',
+    color: 'white',
+    paddingLeft: 10,
   },
 });
